1941acc538afa9ea7a5b972aa535315b28ca632f
Hojin Nam <hojinnam@HojindeMacBook-Pro.local>
2021-06-28 12:27:45 -0700

    Updated the README.md file updated to contain the notes for Django tutorials

--- mysite/django-polls/polls/views.py
+++ mysite/django-polls/polls/views.py
@@ -8,6 +8,7 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
+    # template_name = 'polls/templates/polls/index.html'
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -23,6 +24,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
+    # template_name = 'polls/templates/polls/detail.html'
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
@@ -34,7 +36,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.html'
+    template_name = 'polls/templates/polls/results.html'
 
 
 def vote(request, question_id):
@@ -43,7 +45,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'polls/templates/polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
