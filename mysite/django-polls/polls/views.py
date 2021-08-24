2f0213f9ae8c861a07047d5e7ef414eeacbe525c
Hojin Nam <hojinnam@HojindeMacBook-Pro.local>
2021-08-23 22:01:30 -0700

    fixed the bug that the ResultView path error

--- mysite/django-polls/polls/views.py
+++ mysite/django-polls/polls/views.py
@@ -11,7 +11,6 @@ class IndexView(generic.ListView):
     # template_name = 'polls/templates/polls/index.html'
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
     def get_queryset(self):
         """
         Return the last five published questions (not including those set to be
@@ -26,7 +25,6 @@ class DetailView(generic.DetailView):
     model = Question
     # template_name = 'polls/templates/polls/detail.html'
     template_name = 'polls/detail.html'
-
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -36,11 +34,13 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/templates/polls/results.html'
+    template_name = 'polls/results.html'
+    
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print("hi")
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
